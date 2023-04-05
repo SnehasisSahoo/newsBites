ab6f9d45845dcf0cd8a22d58b86428a67a1cf778
Snehasis Sahoo <sahoo.snehasis2018@gmail.com>
2023-04-05 20:35:15 +0530

    commit: updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,47 @@
 import { Link } from "react-router-dom";
 
 const categoryArray = [
-  "business",
-  "entertainment",
-  "environment",
-  "food",
-  "health",
-  "politics",
-  "science",
-  "sports",
-  "technology",
-  "top",
-  "world",
+	"business",
+	"entertainment",
+	"environment",
+	"food",
+	"health",
+	"politics",
+	"science",
+	"sports",
+	"technology",
+	"top",
+	"world",
 ];
 
 export default function Home() {
-  return (
-    <>
-      <h1 style={{ fontWeight: "bolder", margin: "3vh" }}> News Bites </h1>
-      <hr style={{ border: "1px solid black", margin: "3vh 0" }} />
-      <div className="grid">
-        {categoryArray.map((item, i) => (
-          <div className="card" key={i}>
-            <Link to={`news\\${item}`}><h2>{item}</h2></Link>
-          </div>
-        ))}
-        {/* { categoryArray.map((item,i)=>
+	return (
+		<>
+			<h1 style={{ fontWeight: "bolder", margin: "3vh" }}> News Bites </h1>
+			<hr style={{ border: "1px solid black", margin: "3vh 0" }} />
+			<div className='grid'>
+				{categoryArray.map((item, i) => (
+					<Link to={`news/${item}`} className='card' key={i}>
+							<h2>{item}</h2>
+					</Link>
+				))}
+				{/* { categoryArray.map((item,i)=>
             (<div className="card" key={i}>
               <h2 onClick={()=>{setCategory(item); console.log(item);}}>{item}</h2>
             </div>)
           )} */}
-        <div className="author-card">
-          <h2>
-            <a
-              href="https://www.behance.net/ssahoo"
-              target="_blank"
-              rel="author noreferrer"
-            >
-              Author{" >>"}
-            </a>
-          </h2>
-        </div>
-      </div>
-    </>
-  );
+				<div className='author-card'>
+					<h2>
+						<a
+							href='https://www.behance.net/ssahoo'
+							target='_blank'
+							rel='author noreferrer'
+						>
+							Author{" >>"}
+						</a>
+					</h2>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/News.js
+++ src/components/News.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function News() {
-  /*
+	/*
   INFO: possible categories:
   business
   entertainment
@@ -19,108 +19,128 @@ export default function News() {
   world
   */
 
-  //INFO: url params
+	//INFO: url params
 
-  const { category } = useParams();
+	const { category } = useParams();
 
-  const lang = "en";
-  const [page, setPage] = useState(0);
-  const [res, setRes] = useState([]);
-  const [status, setStatus] = useState("");
+	const lang = "en";
+	const [nextPage, setNextPage] = useState("");
+	const [prevPage, setPrevPage] = useState("");
+	const [page, setPage] = useState("");
+	const [pageNo, setPageNo] = useState(1);
+	const [res, setRes] = useState([]);
+	const [status, setStatus] = useState("");
 
-  const key = "pub_158149fc3caf3020f346e1c3512f775588f53";
-  const URL = `https://newsdata.io/api/1/news?apikey=${key}&category=${category}&language=${lang}`;
-  // const URL = `https://newsdata.io/api/1/news?apikey=${key}&category=${category}&language=${lang}&page=${page}`; //page no not working for some reason
+	const key = "pub_158149fc3caf3020f346e1c3512f775588f53";
+	// const URL = `https://newsdata.io/api/1/news?apikey=${key}&category=${category}&language=${lang}`;
 
-  // let hour = new Date().getHours();
-  const getNews = async () => {
-    try {
-      const news = await axios.get(URL);
-      // console.log(news.data);
-      setRes(news.data.results);
-      // console.log(news.data);
-      setStatus(news.data.status);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	// let hour = new Date().getHours();
+	const getNews = async () => {
+		try {
+			const URL = `https://newsdata.io/api/1/news?apikey=${key}&category=${category}&language=${lang}&page=${page}`;
+			const news = await axios.get(URL);
+			setNextPage(news.data.nextPage);
+			// console.log(news.data);
+			setRes(news.data.results);
+			// console.log(news.data);
+			setStatus(news.data.status);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  useEffect(() => {
-    getNews();
-    alert('API malfunctioning: Pagination out of order.')
-  }, [page]);
+	useEffect(() => {
+		getNews();
+		// alert("API malfunctioning: Pagination out of order.");
+	}, [page]);
 
-  const pageStart = () => {
-    setPage(0);
-  };
+	const pageStart = () => {
+		setPage("");
+		setPageNo(1);
+	};
 
-  const pageEnd = () => {
-    setPage(9);
-  };
+	const pageEnd = () => {
+		setPage(9);
+	};
 
-  const pageUp = () => {
-    setPage(page + 1);
-  };
+	const pageUp = () => {
+		setPrevPage(page);
+		setPage(nextPage);
+		setPageNo(pageNo + 1);
+	};
 
-  const pageDown = () => {
-    setPage(page - 1);
-  };
+	const pageDown = () => {
+		setPage(prevPage);
+		setPageNo(pageNo - 1);
+	};
 
-  return (
-    <>
-      <div className="override">
-        <div className="header">
-          <Link to="/">
-            <h2 className="item">{"< "}home</h2>
-          </Link>
-          <h1 style={{ textTransform: "uppercase" }}>{category}</h1>
-          <div className="pagination">
-            <button className="item" onClick={pageStart} disabled={page <= 0}>
-              {"<<"}
-            </button>
-            <button className="item" onClick={pageDown} disabled={page <= 0}>
-              {"<"}
-            </button>
-            <p className="page-no">{`  ${page + 1}  `}</p>
-            <button className="item" onClick={pageUp} disabled={page >= 9}>
-              {">"}
-            </button>
-            <button className="item" onClick={pageEnd} disabled={page >= 9}>
-              {">>"}
-            </button>
-          </div>
-        </div>
-        <dl>
-          {res.length > 0 ? 
-            <>
-            {res.map((data, i) => (
-              <div key={i} className="news">
-                <dt>
-                  <h3>
-                    {page * 10 + i + 1}
-                    {". "}
-                    {data.title}
-                  </h3>
-                </dt>
-                <dd>
-                  <b>Breif: </b>
-                  {data.description == null
-                    ? "description not found"
-                    : data.description}
-                </dd>
-                <a href={data.link} target="_blank" rel="noreferrer">
-                  {" "}
-                  read more{" >>"}{" "}
-                </a>
-                <hr />
-              </div>
-            ))}
-            </>:
-            <>
-              <h2> loading ....</h2>
-            </>}
-        </dl>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='override'>
+				<div className='header'>
+					<Link to='/'>
+						<h2 className='item'>{"< "}home</h2>
+					</Link>
+					<h1 style={{ textTransform: "uppercase" }}>{category}</h1>
+					<div className='pagination'>
+						<button className='item' onClick={pageStart} disabled={pageNo === 1}>
+							{"<<"}
+						</button>
+						<button
+							hidden
+							className='item'
+							onClick={pageDown}
+							disabled={pageNo === 1}
+						>
+							{"<"}
+						</button>
+						<p className='page-no'>{`  ${pageNo}  `}</p>
+						<button className='item' onClick={pageUp} disabled={page >= 9}>
+							{">"}
+						</button>
+						<button
+							hidden
+							className='item'
+							onClick={pageEnd}
+							disabled={page >= 9}
+						>
+							{">>"}
+						</button>
+					</div>
+				</div>
+				<dl>
+					{res.length > 0 ? (
+						<>
+							{res.map((data, i) => (
+								<div key={i} className='news'>
+									<dt>
+										<h3>
+											{(pageNo-1) * 10 + (i+1)}
+											{". "}
+											{data.title}
+										</h3>
+									</dt>
+									<dd>
+										<b>Breif: </b>
+										{data.description == null
+											? "description not found"
+											: data.description}
+									</dd>
+									<a href={data.link} target='_blank' rel='noreferrer'>
+										{" "}
+										read more{" >>"}{" "}
+									</a>
+									<hr />
+								</div>
+							))}
+						</>
+					) : (
+						<>
+							<h2> loading ....</h2>
+						</>
+					)}
+				</dl>
+			</div>
+		</>
+	);
 }
